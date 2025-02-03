27aa46e4fda4ef5620e94682df0703e6b028f7fe
Andrea Scardigli <andrea.scardigli@eng.it>
2025-02-03 16:08:39 +0100

    Release 1.11.0 (#5)
    * Update project to latest tag verificafirma-eidas-beans-1.11.0
    
    * Update SNAPSHOT version
    
    * [maven-release-plugin] prepare release verificafirma-eidas-beans-1.11.0
    
    * [maven-release-plugin] prepare for next development iteration
    
    * Update SNAPSHOT version
    
    * [maven-release-plugin] prepare release verificafirma-eidas-beans-1.11.0
    
    ---------
    
    Co-authored-by: parerworker <DevPARER@regione.emilia-romagna.it>
    Co-authored-by: GitHub Actions <actions@github.com>

--- src/main/java/it/eng/parer/eidas/model/EidasWSReportsDTOTree.java
+++ src/main/java/it/eng/parer/eidas/model/EidasWSReportsDTOTree.java
@@ -22,13 +22,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-
 import eu.europa.esig.dss.ws.validation.dto.WSReportsDTO;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 @XmlRootElement(name = "EidasWSReportsDTO", namespace = "http://parer.regione.emilia-romagna.it/validation/")
 @XmlAccessorType(XmlAccessType.FIELD)
